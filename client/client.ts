1c0f231a380e223ddb5478bb74b8bfb4181b318b
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-04-04 07:07:40 -0300

    refactor: remove not used code

--- client/client.ts
+++ client/client.ts
@@ -5,55 +5,6 @@ var DEFAULT_DISTANCE = 8;
 let INCREASE_CHECKPOINT_SPACE_BUTTON = 39;
 let DECREASE_CHECKPOINT_SPACE_BUTTON = 40;
 
-RegisterCommand("getpos", ()=> {
-	var DEFAULT_DISTANCE = 8
-	let playerEntity = GetPlayerPed(PlayerId());
-	let coords;
-
-	coords = GetEntityCoords(playerEntity, true);
-
-	let z = coords[2];
-	let y = coords[1];
-	let x = coords[0];
-
-	let heading = GetEntityHeading(playerEntity);
-
-	console.log(heading);
-
-	let angLeftToRads = getLeftHeading(heading) * 3.14 / 180;
-	let angRightToRads = getRightHeading(heading) * 3.14 / 180;
-
-	let newXLeft = x + DEFAULT_DISTANCE * Math.cos(angLeftToRads-1.5)
-	let newYLeft = y + DEFAULT_DISTANCE * Math.sin(angLeftToRads-1.5)
-
-	let newXRight = x + DEFAULT_DISTANCE * Math.cos(angRightToRads-1.5)
-	let newYRight = y + DEFAULT_DISTANCE * Math.sin(angRightToRads-1.5)
-
-	console.log("LEFT ANG ", getLeftHeading(heading), " | LEFT RAD ", angLeftToRads);
-	console.log("RIGHT ANG ", getRightHeading(heading), " | RIGHT RAD ", angRightToRads);
-
-	CreateObject(
-		-73333162, //SEM LISTRA
-		newXLeft,
-		newYLeft,
-		z,
-		true,
-		false,
-		false,
-	);
-
-	CreateObject(
-		-534360227, //COM LISTRA
-		newXRight,
-		newYRight,
-		z,
-		true,
-		false,
-		false,
-	);
-
-}, false);
-
 function getLeftHeading(headingValue: number) {
 	let result = headingValue - 90;
 
