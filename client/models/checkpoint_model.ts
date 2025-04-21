ae153bb6c544790de7caf8d5406b2931b6a11b3f
Ohlavrac <joaovicrcarvalho@gmail.com>
2025-04-21 10:46:08 -0300

    feat: add temp checkpoint model

---
+++ client/models/checkpoint_model.ts
@@ -0,0 +1,6 @@
+interface CheckpointModel {
+    checkpointIndex: number;
+    coordLeft: [number, number, number];
+    coordRight: [number, number, number];
+    coordCenter: [number, number];
+};
